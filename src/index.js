58863b8e0000ba428471b45924efb264c7efff27
br0r <viktor@silfverstrom.com>
2015-12-01 11:40:04 +0100

    added tracker for session

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import request from 'superagent';
 
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
 class Microstar {
 
     init(config) {
         this.config = config;
 
-        const _session_var = (config.sessionVariables) ? config.sessionVariables : null;
+        const _session_var = (config.sessionVariables) ? config.sessionVariables : {};
+        _session_var._session_id = guid();
+        _session_var._session_event = 0;
         this.$_session_variables = _session_var;
     }
 
-    track(event_name, event_data) {
+    _increaseSessionEvent() {
+      if (this.$_session_variables && typeof this.$_session_variables._session_event !== 'undefined') {
+        this.$_session_variables._session_event += 1;
+      }
+    }
+
+    track(event_name, event_data = {}) {
         const config = this.config;
         const trackPath = config.rootPath.replace(/\/$/, '') + '/track';
         const screen = window.screen || {};
@@ -37,6 +55,7 @@ class Microstar {
                     console.error('Error when performing track', err);
                     return;
                 }
+                this._increaseSessionEvent();
             });
     }
 };
